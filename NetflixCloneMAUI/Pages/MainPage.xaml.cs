ff382e1f8f249bac97f345bba80d4a490395d2cf
trình dương <duongtrinhbg2003@gmail.com>
2023-11-21 22:29:14 +0700

    create services and api key

--- NetflixCloneMAUI/Pages/MainPage.xaml.cs
+++ NetflixCloneMAUI/Pages/MainPage.xaml.cs
@@ -1,12 +1,22 @@
-﻿namespace NetflixCloneMAUI
+﻿using NetflixCloneMAUI.Services;
+
+namespace NetflixCloneMAUI.Pages
 {
     public partial class MainPage : ContentPage
     {
+        private readonly TmdbService _tmdbService;
         int count = 0;
 
-        public MainPage()
+        public MainPage(TmdbService tmdbService)
         {
             InitializeComponent();
+            _tmdbService = tmdbService;
+        }
+
+        protected async override  void OnAppearing()
+        {
+            base.OnAppearing();
+            var trending = await _tmdbService.GetTrendingAsync();
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
